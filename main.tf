b93fe3e89b5d3b8f1fe67a794ac3009bc5ddb257
Guy Barros <38210175+GuyBarros@users.noreply.github.com>
2021-01-04 21:39:53 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,9 +10,47 @@ resource "tls_self_signed_cert" "root" {
   private_key_pem = "${tls_private_key.root.private_key_pem}"
 
   subject {
-   # common_name  = "${var.common_name}"
+    common_name  = "${var.common_name}"
     organization = "${var.organization}"
   }
+  
+  
+  dns_names = [
+    # Consul
+    "${var.namespace}-server-${count.index}.node.consul",
+    "${var.namespace}-server-${count.index}.node.${var.region}.consul",
+
+    "*.service.consul",
+    "*.service.${var.region}.consul",
+    "*.query.consul",
+    "consul.service.consul",
+
+    # Nomad
+    "nomad.service.consul",
+    "nomad.service.${var.region}.consul",
+
+
+    "client.global.nomad",
+    "server.global.nomad",
+
+    # Vault
+    "vault.service.consul",
+    "vault.query.consul",
+    "active.vault.service.consul",
+    "active.vault.service.${var.region}.consul",
+    "standby.vault.service.consul",
+    "standby.vault.service.${var.region}.consul",
+    "performance-standby.vault.service.consul",
+    "performance-standby.vault.service.${var.region}.consul",
+
+    # Common
+    "localhost",
+    "*.${var.namespace}.${data.aws_route53_zone.fdqn.name}",
+    "server-0.eu-guystack.original.aws.hashidemos.io",
+    "server-1.eu-guystack.original.aws.hashidemos.io",
+    "server-2.eu-guystack.original.aws.hashidemos.io",
+  ]
+
 
   validity_period_hours = "${var.validity_period_hours}"
 
