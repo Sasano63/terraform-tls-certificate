9c139af48543ee8a7f5b7e0a8c2a2ee0e61b0997
Sasano63 <sasano@hashicorp.com>
2021-09-28 09:10:12 +0200

    output sensitive

--- outputs.tf
+++ outputs.tf
@@ -5,13 +5,13 @@
 
 output "ca_private_key_pem" {
   value = "${tls_private_key.root.private_key_pem}"
-  sensitive = false
+  sensitive = true
 
 }
 
 output "ca_cert_pem" {
   value = "${tls_self_signed_cert.root.cert_pem}"
-  sensitive = false
+  sensitive = true
 
 }
 
